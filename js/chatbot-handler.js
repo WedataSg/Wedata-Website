fe95f663fd5bdb058a588131135b46e4382e86f6
WedataSg <102406501+WedataSg@users.noreply.github.com>
2022-03-27 15:57:04 +0800

    Update chatbot-handler.js

--- js/chatbot-handler.js
+++ js/chatbot-handler.js
@@ -1,10 +1,3 @@
-//Recolor the chatbot header
-headers = document.getElementById("d8iframe").document.getElementsByTagName("header")
-Array.prototype.forEach.call(headers, function(el) {
-	el.style.backgroundColor = "#787E7D";
-});
-
-
 function openChatbot(){
 	document.getElementById("chatbot-div").style.visibility = "visible";
 	document.getElementById("chatbot-img").style.visibility = "hidden";
